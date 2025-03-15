3fcaf119251b9f1c1886e6de9261ca0e730827e7
ETO <130556647+ETO-QSH@users.noreply.github.com>
2025-03-15 17:56:29 +0800

    Update L1-005 考试座位号.py

--- 团体程序设计天梯赛-练习集-L1/L1-005 考试座位号.py
+++ 团体程序设计天梯赛-练习集-L1/L1-005 考试座位号.py
@@ -1,9 +1,4 @@
+numL = [list(map(int, input().split())) for k in range(int(input()))]
 i = int(input())
-
-numL = [list(map(int, input().split())) for k in range(i)]
-
-i = int(input())
-
 lst = list(map(int, input().split()))
-
 [print(j[0], j[2]) for i in lst for j in numL if j[1] == i]
