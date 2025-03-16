5addb7db76c9dc35d2a9ccfe1ae57757412dd6a3
ETO <130556647+ETO-QSH@users.noreply.github.com>
2025-03-16 10:53:10 +0800

    Update L1-099 帮助色盲.py

--- 团体程序设计天梯赛-练习集-L1/L1-099 帮助色盲.py
+++ 团体程序设计天梯赛-练习集-L1/L1-099 帮助色盲.py
@@ -2,8 +2,7 @@ n, m = map(int, input().split())
 
 if n in [0, 1]:
     if m == 1: print("-")
-    else: print("biii") if n == 0 else print("dudu")
+    else: print("biii" if n == 0 else "dudu")
 else: print("-")
 
-if n in [0, 2]: print("stop")
-else: print("move")
+print("stop" if n in [0, 2] else "move")
